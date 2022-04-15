6b3225f13821d4bd0c20c1824855a4b08d2e43f6
Novitz Jean-Philippe <novitz@gmail.com>
2022-04-16 00:39:26 +0200

    meta description refactor

--- web/app/plugins/jphn_seo/jphn_seo.php
+++ web/app/plugins/jphn_seo/jphn_seo.php
@@ -10,20 +10,21 @@
 
 
 add_action('wp_head', 'add_title_from_h1');
-add_action('wp_head', 'activate_excerpt');
+add_action('wp_head', 'add_description_from_tagline');
 add_post_type_support( 'page', 'excerpt' );
 add_theme_support( 'block-templates' );
 
 function add_title_from_h1(){
-//    var_dump(); die;
-    if (!$title = get_the_title()) $title = get_bloginfo();
+//    var_dump(get_the_content()); die;
+    if (!$title = get_the_title()) $title = get_bloginfo('name');
 
     echo '<title>' .$title.' </title>';
 }
 
-function activate_excerpt()
+function add_description_from_tagline()
 {
-    if (!$description = get_the_excerpt()) $title = bloginfo('description');
+    if (!$description = get_the_excerpt()) $description = get_bloginfo('description');
+    if (strlen($description) < 1)  $description = substr(get_the_content(), 0, 200);
         echo '<meta name="description" content="' . $description . ' - Liège Web" />' . "\n";
 
 }
