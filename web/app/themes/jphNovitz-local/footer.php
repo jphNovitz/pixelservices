2f3bac5e2fc926d366f1959e0761494acb316768
Novitz Jean-Philippe <novitz@gmail.com>
2022-04-24 02:06:01 +0200

    reformat footer with centered li

--- web/app/themes/jphNovitz-local/footer.php
+++ web/app/themes/jphNovitz-local/footer.php
@@ -8,14 +8,22 @@
 
 </div>
 
-<footer class="site-footer w-full inline-flex justify-center mt-40 p-12">
-       <p>&copy 2022, LGWeb.be.  Réalisation de sites internet à Liège (<a href="#">Conditions d'utilisation</a>) ~ Suivez nous sur les réseaux sociaux:
-       <a href="#" class="bg-blue-600 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i class="fab fa-facebook-f"></i> </a>
-    <a href="#" class="bg-slate-900 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i class="fab fa-instagram"></i> </a>
-           <a href="#" class="bg-blue-600 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i class="fab fa-linkedin-in"></i> </a>
-<!--    <article class="flex flex-1 p-4">1</article>-->
-<!--    <article class="flex flex-1 p-4">2</article>-->
-<!--    <article class="flex flex-1 p-4" >3</article>-->
+<footer class="site-footer w-full p-12">
+    <ul>
+        <li class="text-center">&copy 2022, LGWeb.be. Réalisation de sites internet à Liège (<a href="#">Conditions d'utilisation</a>)</li>
+        <li class="text-center">Suivez nous sur les réseaux sociaux:</li>
+        <li class="text-center">
+            <a href="#" class="bg-blue-600 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i
+                        class="fab fa-facebook-f"></i> </a>
+            <a href="#" class="bg-slate-900 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i
+                        class="fab fa-instagram"></i> </a>
+            <a href="#" class="bg-blue-600 text-white rounded-full inline-flex justify-center items-center w-6 h-6"> <i
+                        class="fab fa-linkedin-in"></i> </a>
+        </li>
+    </ul>
+    <!--    <article class="flex flex-1 p-4">1</article>-->
+    <!--    <article class="flex flex-1 p-4">2</article>-->
+    <!--    <article class="flex flex-1 p-4" >3</article>-->
 
 </footer>
 
