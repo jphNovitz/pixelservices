98e49022f22d3e8cf74903c402b054d763a5d57a
Novitz Jean-Philippe <novitz@gmail.com>
2022-11-11 00:32:32 +0100

    remove all

--- web/app/plugins/jphn_seo/views/front/add_title_from_h1.php
+++
@@ -1,7 +0,0 @@
-<!--
-Displays title and pg:title metas in page heead
-package: jphn_seo
-
--->
-<title><?php echo $GLOBALS['jphn_seo_title']?></title > &#13;
-<meta property = "og:title" content = "<?php echo $GLOBALS['jphn_seo_title']?>" /> &#13;
