cdc3667bbf3ce7db2f0d6ac843803066a8e6d855
Novitz Jean-Philippe <novitz@gmail.com>
2022-11-24 22:55:48 +0100

    Remove Login link (commented)

--- assets/js/components/hero.vue
+++ assets/js/components/hero.vue
@@ -24,7 +24,7 @@
           </div>
           <div class="flex lg:hidden">
             <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-              <span class="sr-only">Open main menu</span>
+              <span class="sr-only">Ouvrir le menu</span>
               <Bars3Icon class="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
@@ -32,7 +32,7 @@
             <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-semibold text-gray-900 hover:text-gray-900">{{ item.name }}</a>
           </div>
           <div class="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
-            <a href="#" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log in</a>
+            <!-- <a href="#" class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log in</a> -->
           </div>
         </nav>
         <Dialog as="div" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -41,12 +41,14 @@
               <div class="flex">
                 <a href="#" class="-m-1.5 p-1.5">
                   <span class="sr-only">Pixel Services</span>
-                  <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                  <span class="font-black text-4xl pr-3">PI<SparklesIcon  class="w-7 inline-flex text-indigo-600"/>EL </span>
+              <span class="italic text-3xl">Services</span>
+                  <!-- <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> -->
                 </a>
               </div>
               <div class="flex">
                 <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-                  <span class="sr-only">Close menu</span>
+                  <span class="sr-only">Fermer le menu</span>
                   <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                 </button>
               </div>
@@ -57,7 +59,7 @@
                   <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10">{{ item.name }}</a>
                 </div>
                 <div class="py-6">
-                  <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a>
+                  <!-- <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10">Log in</a> -->
                 </div>
               </div>
             </div>
