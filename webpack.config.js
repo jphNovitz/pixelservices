4cb847c5b59c443f9719fc49e56f455814124407
Novitz Jean-Philippe <novitz@gmail.com>
2022-11-25 21:10:27 +0100

    runtime compiler false

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ Encore
 
     .enableVueLoader(function (options) {
         options.preLoaders = { 
+            runtimeCompilerBuild: false,
             delimiters: ['${', '}$']
          }
     })
