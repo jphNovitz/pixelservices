2728f6d31aac332224a790f85a6815d03852db84
Novitz Jean-Philippe <novitz@gmail.com>
2022-11-25 23:39:55 +0100

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero': "linear-gradient(rgba(248, 250, 252, .8), rgba(248, 250, 252, 0.6), rgba(248, 250, 252, 1)), url('images/liegeunsplash.webp')",
+        'hero': "linear-gradient(rgba(248, 250, 252, .8), rgba(248, 250, 252, 0.6), rgba(248, 250, 252, 1))",
         'hero-mobile': "linear-gradient(rgba(248, 250, 252, .8), rgba(248, 250, 252, 0.6), rgba(248, 250, 252, 1)), url('images/liegeunsplash-s-2.webp')",
         'hero-md': "linear-gradient(rgba(248, 250, 252, .8), rgba(248, 250, 252, 0.6), rgba(248, 250, 252, 1)), url('images/liegeunsplash-md.webp')",
       }
