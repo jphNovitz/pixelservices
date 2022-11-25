3acae0e47be8f6a924b37451f3b33e7f93997580
Novitz Jean-Philippe <novitz@gmail.com>
2022-11-25 22:26:35 +0100

    ...

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -36,13 +36,29 @@ class HomeController extends AbstractController
             'form' => $form->createView(),
         ]);
 
-        // cache publicly for 3600 seconds
-        $response->setPublic();
-        $response->setMaxAge(3600);
-    
+        // // cache publicly for 3600 seconds
+        // $response->setPublic();
+        // $response->setMaxAge(3600);
+        // use this method to set several cache settings in one call
+        // (this example lists all the available cache settings)
+        $response->setCache([
+            'must_revalidate'  => false,
+            'no_cache'         => false,
+            'no_store'         => false,
+            'no_transform'     => false,
+            'public'           => true,
+            'private'          => false,
+            'proxy_revalidate' => false,
+            'max_age'          => 6000,
+            's_maxage'         => 6000,
+            'immutable'        => true,
+            'last_modified'    => new \DateTime(),
+            'etag'             => 'abcdef'
+        ]);
+
         // (optional) set a custom Cache-Control directive
         $response->headers->addCacheControlDirective('must-revalidate', true);
-    
+
         return $response;
 
         // return $this->render('home/index.html.twig', [
