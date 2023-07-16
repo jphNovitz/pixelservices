6b1ae9270980bb76a7335b23b2d28854b1cea09b
Novitz Jean-Philippe <novitz@gmail.com>
2023-07-17 01:04:13 +0200

    new : (TWIG COMPONENT) Blog

---
+++ src/Components/Blog/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Components\Blog;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: 'components/blog/Article.html.twig')]
+class Article
+{
+    public string $image_url = "";
+    public string $image_alt = "";
+    public string $title = "";
+    public string $content = "";
+
+}
