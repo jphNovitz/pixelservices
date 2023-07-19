b1ae27e68d22da553c595635801c93f10925db88
Novitz Jean-Philippe <novitz@gmail.com>
2023-07-20 01:54:34 +0200

    new : event subscriber Listen to update / persist Blog and add custom classes to html tags

---
+++ src/EventSubscriber/TextStyleEasyAdminSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Blog;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class TextStyleEasyAdminSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['addCustomClassesToContentBeforePersist'],
+            BeforeEntityUpdatedEvent::class => ['addCustomClassesToContentBeforeUpdate'],
+        ];
+    }
+
+    public function addCustomClassesToContentBeforePersist(BeforeEntityPersistedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Blog)) {
+            return;
+        }
+
+//        $content = preg_replace('/<ol>/', '<ol class="my-custom-class">', $entity->getContent());
+        $entity->setContent($this->addCustomClasses($entity->getContent()));
+    }
+    public function addCustomClassesToContentBeforeUpdate(BeforeEntityUpdatedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Blog)) {
+            return;
+        }
+
+        $entity->setContent($this->addCustomClasses($entity->getContent()));
+    }
+
+    public function addCustomClasses($content){
+        $content = preg_replace('/<ol>/', '<ol class="my-custom-class">', $content);
+        return $content ;
+
+    }
+}
