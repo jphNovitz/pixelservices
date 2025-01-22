dc82457607442fbe3a26432804805407e3d92ca3
Novitz Jean-Philippe <novitz@gmail.com>
2025-01-22 16:11:33 +0100

    update Project Controller Crud
    remode id field

--- src/Controller/Admin/Project/ProjectCrudController.php
+++ src/Controller/Admin/Project/ProjectCrudController.php
@@ -20,7 +20,6 @@ class ProjectCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextField::new('header'),
             TextField::new('link'),
