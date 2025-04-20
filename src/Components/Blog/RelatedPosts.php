fe180508833af5829a5fba8a7c3238a66910efa7
Novitz Jean-Philippe <novitz@gmail.com>
2025-04-20 05:19:58 +0200

    Add RelatedPosts component to display related articles in blog posts

---
+++ src/Components/Blog/RelatedPosts.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Components\Blog;
+
+use App\Entity\Topic;
+use App\Repository\TopicRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: 'components/blog/related-posts.html.twig')]
+class RelatedPosts
+{
+    public ?Topic $relatedTopic = null;
+    public array $relatedPosts = [];
+    public int $topicId = 0;
+    public int  $currentPostId = 0;
+
+    public function __construct(private TopicRepository $topicRepository)
+    {
+    }
+    public function mount(int $topicId): void
+    {
+        $this->relatedTopic = $this->topicRepository->findOneBy(['id' => $topicId]);
+    }
+
+}
