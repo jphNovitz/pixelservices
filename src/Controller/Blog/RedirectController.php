38e8de16f09664ed9025f48c493c14fa62bfd87a
Novitz Jean-Philippe <novitz@gmail.com>
2025-07-28 00:10:08 +0200

    refactor: update RedirectController namespace and enhance index.html.twig layout

--- src/Controller/Blog/RedirectController.php
+++ src/Controller/Blog/RedirectController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Blog;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
