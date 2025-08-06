5b2339f8c90a6c6bf2272ab165f8be2e1f512d46
Novitz Jean-Philippe <novitz@gmail.com>
2025-08-06 16:40:24 +0200

    install CKEDITOR

--- src/Controller/Admin/Blog/TopicCrudController.php
+++ src/Controller/Admin/Blog/TopicCrudController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Admin\Blog;
 
 use App\Entity\Topic;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Translation\TranslatableMessage;
 
 class TopicCrudController extends AbstractCrudController
@@ -17,21 +19,24 @@ class TopicCrudController extends AbstractCrudController
         return Topic::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        $crud->setFormThemes(['@FOSCKEditor/Form/ckeditor_widget.html.twig', '@EasyAdmin/crud/form_theme.html.twig']);
+        return $crud;
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('title'),
             TextField::new('summary')->setColumns('20'),
-            TextEditorField::new('content')->setTrixEditorConfig([
-                'blockAttributes' => [
-                    'default' => ['tagName' => 'p'],
-                    'heading1' => ['tagName' => 'h3']
-                ]]),
-             ImageField::new('image', new TranslatableMessage('Image'))
-                 ->setUploadDir('public/images/topic/')
-                 ->setBasePath('public/images/topic/'),
-            ];
+            TextEditorField::new('content')
+                ->setFormType(CKEditorType::class)
+                ->hideOnIndex(),
+            ImageField::new('image', new TranslatableMessage('Image'))
+                ->setUploadDir('public/images/topic/')
+                ->setBasePath('public/images/topic/'),
+        ];
     }
 
 }
