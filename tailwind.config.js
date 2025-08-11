439f01fd8113d9e4e781121ecd78d1e71f47c54f
Novitz Jean-Philippe <novitz@gmail.com>
2025-08-11 04:47:12 +0200

    Add bg image for mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
             backgroundImage: {
                 'hero': "linear-gradient(rgba(248, 250, 252, .8), rgba(248, 250, 252, 0.6), rgba(248, 250, 252, 1))",
                 'hero-laptop': "url('../images/hero/hero_laptop_jphiweb.webp')",
+                'hero-laptop-vertical': "url('../images/hero/hero_laptop_vertical_jphiweb.webp')",
                 'base_share': "url('../images/jphiweb_facebook_image.webp')",
                 'hero-mobile': "url('../images/st-antoine-sart-messire-guillaume.webp')",
                 'hero-md': "url('../images/st-antoine-sart-messire-guillaume.webp')",
