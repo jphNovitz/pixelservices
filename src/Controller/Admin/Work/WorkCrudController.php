e6942edc16154620412f40b1ac1c3708500aaa62
Novitz Jean-Philippe <novitz@gmail.com>
2025-12-18 19:58:14 +0100

    Add maxlength attributes to Work entity fields in CRUD controller

--- src/Controller/Admin/Work/WorkCrudController.php
+++ src/Controller/Admin/Work/WorkCrudController.php
@@ -48,15 +48,16 @@ class WorkCrudController extends AbstractCrudController
     {
         return [
 //            IdField::new('id'),
-            TextField::new('name', new TranslatableMessage('Name'))->setColumns(12),
+            TextField::new('name', new TranslatableMessage('Name'))->setColumns(12)
+                ->setFormTypeOption('attr', ['maxlength' => 255]),
             TextField::new('seoTitle')
                 ->setColumns('20')
                 ->setLabel(new TranslatableMessage('SEO Title'))
                 ->setHelp('Max 65 caractères')
                 ->setFormTypeOption('attr', ['maxlength' => 65]),
-            TextField::new('slugTitle')->setColumns('20')->setLabel(new TranslatableMessage('Slug Title')),
+            TextField::new('slugTitle')->setColumns('20')->setLabel(new TranslatableMessage('Slug Title'))->setFormTypeOption('attr', ['maxlength' => 65]),
             TextField::new('seoDescription', new TranslatableMessage('Meta Description'))->setColumns(12)
-                ->setMaxLength(160),
+                ->setMaxLength(160)->setFormTypeOption('attr', ['maxlength' => 160]),
             TextField::new('description_short', new TranslatableMessage('Description_short')),
             TextEditorField::new('description_long', new TranslatableMessage('Description_long'))
                 ->setFormType(CKEditorType::class)
