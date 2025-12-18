19f4978f59bfc1f0921c86fd7c9bef58729a42e3
Novitz Jean-Philippe <novitz@gmail.com>
2025-12-18 19:23:22 +0100

    Add SEO fields to Work entity and update seo_summary to seo_description

---
+++ migrations/Version20251218173950.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251218173950 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE work ADD seo_title VARCHAR(65) DEFAULT NULL, ADD slug_title VARCHAR(65) DEFAULT NULL, ADD seo_summary VARCHAR(160) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE work DROP seo_title, DROP slug_title, DROP seo_summary');
+    }
+}
