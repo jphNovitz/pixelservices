232f0f29ff7bcfcf4cd12b2a85b1b7ed7de46821
Novitz Jean-Philippe <novitz@gmail.com>
2025-12-18 21:36:40 +0100

    Update slug generation to use slugTitle instead of seoTitle

--- src/Entity/Work.php
+++ src/Entity/Work.php
@@ -63,7 +63,7 @@ class Work
     private bool $active = false;
 
     #[ORM\Column(length: 128, unique: true)]
-    #[Gedmo\Slug(fields: ['seoTitle'])]
+    #[Gedmo\Slug(fields: ['slugTitle'])]
     private ?string $slug = null;
 
     #[ORM\Column(length: 255, nullable: true)]
